8745f5c02a44cd7a8427d1b21ed8a2a6ea9c0dad
ArgentumHeart <argentumheart@gmail.com>
2016-02-22 22:24:36 +0300

    +2

--- src/main/java/ocr_sign.java
+++ src/main/java/ocr_sign.java
@@ -7,13 +7,36 @@ import org.apache.pdfbox.rendering.PDFRenderer;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class ocr_sign {
 
     private static final String TessdataPath = "./res/tessdata";
     private static final String pdfFilePath = "./res/SOMECOMPANY.pdf";
 
+    public static HashMap<String, String> extractData(String source, ArrayList<String> keys) {
+        HashMap<String, String> data = new HashMap<>();
+        String temp;
+        String tempsource = source;
+        String tempvalue;
+        do {
+            temp = tempsource.substring(0,tempsource.indexOf("\n"));
+            for (String key : keys) {
+                if (temp.contains(key + ":")) {
+                    tempvalue = temp.substring(temp.indexOf(":") + 2, temp.length()); //2 is a lazy way to remove a 1 space symbol :D
+                    data.put(key, tempvalue);
+                    System.out.println("Extracted " + key + " = " + tempvalue);
+                }
+            }
+            tempsource = tempsource.substring(tempsource.indexOf("\n")+1);
+        } while (tempsource.length() > 0);
+
+        return data;
+    }
+
+    //All pdf splitting/rendering and ocr in main(looks really bad :D ) since i have no idea about how this will look like after merging into alfresco.
     public static void main(String[] args) {
         try {
             //
@@ -41,13 +64,26 @@ public class ocr_sign {
             //File testImage = new File("./res/1.png");
             String result1 = instance.doOCR(image);
             String result2 = instance.doOCR(image,new Rectangle(3202,2039,1280,106));
-            System.out.println(result1);
-            System.out.println(result2);
 
-            //Working with metadata
+            //System.out.println(result1);
+            //System.out.println(result2);
+
+            ArrayList<String> keys = new ArrayList<>();
+            keys.add("Due");
+            keys.add("From");
+            keys.add("To");
+
+            HashMap<String, String> obtainedData = extractData(result1, keys);
+            obtainedData.put("Total", result2.substring(result2.lastIndexOf(":")));
+
+
+            //testing APIs for metadata
+            //PDDocumentInformation metadata = document.getDocumentInformation();
+            //metadata.setCustomMetadataValue("testkey", "testvalue");
+            //metadata.setAuthor("Test Author");
+
             PDDocumentInformation metadata = document.getDocumentInformation();
-            metadata.setCustomMetadataValue("testkey", "testvalue");
-            metadata.setAuthor("Test Author");
+            obtainedData.forEach((k,v) -> metadata.setCustomMetadataValue(k,v)); //
             document.setDocumentInformation(metadata);
 
             //writing new PDF to FS

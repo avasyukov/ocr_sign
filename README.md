1fd8b84471d52b4b48b21c5b31550b2bbda344ca
ArgentumHeart <argentumheart@gmail.com>
2016-02-22 20:16:43 +0300

    +1

---
+++ README.md
@@ -0,0 +1,10 @@
+Ghost4j ( java wrapper for Ghostscript ) is outdated. They're trying to use com.sun.jna 3, which is old af. (and not compatible with new version of JNA)
+
+TODO: - Train tesseract for perfect OCR ( https://github.com/tesseract-ocr/tesseract/wiki/TrainingTesseract )
+      -     
+
+
+.Apache PDFBox for pdf manipulation (metadata, splitting in pages, rendering, signing etc) 
+.Tess4J for pure OCR
+
+
---
+++ src/main/java/ocr_sign.java
@@ -0,0 +1,76 @@
+import net.sourceforge.tess4j.Tesseract;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentInformation;
+import org.apache.pdfbox.rendering.PDFRenderer;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.security.KeyStore;
+
+public class ocr_sign {
+
+    private static final String TessdataPath = "./res/tessdata";
+    private static final String pdfFilePath = "./res/SOMECOMPANY.pdf";
+
+    public static void main(String[] args) {
+        try {
+            //
+            File pdfFile = new File(pdfFilePath);
+            System.out.println(pdfFile.exists());
+
+            //
+            PDDocument document = PDDocument.load(pdfFile);
+            System.out.println("Pagecount = " + document.getNumberOfPages());
+
+            //
+            PDFRenderer renderer = new PDFRenderer(document);
+            BufferedImage image = renderer.renderImageWithDPI(0,600);
+
+            /*
+            //test image to check if everything went fine
+            File TestOutput = new File ("./res/output.png");
+            ImageIO.write(image, "png", TestOutput);
+            */
+
+            //OCR stuff with prepared image
+            Tesseract instance = new Tesseract();
+            instance.setDatapath(TessdataPath);
+            instance.setLanguage("eng");
+            //File testImage = new File("./res/1.png");
+            String result1 = instance.doOCR(image);
+            String result2 = instance.doOCR(image,new Rectangle(3202,2039,1280,106));
+            System.out.println(result1);
+            System.out.println(result2);
+
+            //Working with metadata
+            PDDocumentInformation metadata = document.getDocumentInformation();
+            metadata.setCustomMetadataValue("testkey", "testvalue");
+            metadata.setAuthor("Test Author");
+            document.setDocumentInformation(metadata);
+
+            //writing new PDF to FS
+            String name = pdfFile.getName();
+            String outputName = name.substring(0, name.lastIndexOf(".")) + "_updated.pdf";
+            File outputFile = new File(pdfFile.getParent(), outputName);
+            document.save(outputFile);
+            document.close();
+
+
+            /*
+            //Let's check that new pdf has that (testkey,testvalue)
+            document.load(new File(pdfFileOutputPath));
+            metadata = document.getDocumentInformation();
+            System.out.println(metadata.getCustomMetadataValue("testkey"));
+            document.close();
+            */
+
+            String[] arguments = {"/home/argentum/keystore.p12", "123456", outputFile.getCanonicalPath()};
+            CreateSignature.main(arguments);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
